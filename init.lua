70ff7102e399ca2b75f2c56d0006202490d8f599
Nathan Dumar <njdumar@gmail.com>
2025-03-27 12:52:52 -0500

    Change ruff to use single quotes

--- init.lua
+++ init.lua
@@ -770,11 +770,17 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'ruff_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        ruff_format = {
+          append_args = { '--config', "format.quote-style = 'single'" },
+        },
       },
     },
   },
